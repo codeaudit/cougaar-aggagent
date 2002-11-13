c84d22f49344765eb6df3b395296010151d7a370
ahelsing <ahelsing>
2002-11-13 21:27:14 +0000

    Add copyright Fix another use of PluginBindingSite

--- aggagent/src/org/cougaar/lib/aggagent/servlet/BlackboardServletComponent.java
+++ aggagent/src/org/cougaar/lib/aggagent/servlet/BlackboardServletComponent.java
@@ -1,3 +1,23 @@
+/* 
+ * <copyright>
+ * Copyright 2002 BBNT Solutions, LLC
+ * under sponsorship of the Defense Advanced Research Projects Agency (DARPA).
+
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the Cougaar Open Source License as published by
+ * DARPA on the Cougaar Open Source Website (www.cougaar.org).
+
+ * THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS
+ * PROVIDED 'AS IS' WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR
+ * IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND WITHOUT
+ * ANY WARRANTIES AS TO NON-INFRINGEMENT.  IN NO EVENT SHALL COPYRIGHT
+ * HOLDER BE LIABLE FOR ANY DIRECT, SPECIAL, INDIRECT OR CONSEQUENTIAL
+ * DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE OF DATA OR PROFITS,
+ * TORTIOUS CONDUCT, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ * PERFORMANCE OF THE COUGAAR SOFTWARE.
+ * </copyright>
+ */
 package org.cougaar.lib.aggagent.servlet;
 
 import java.io.IOException;
@@ -159,17 +179,9 @@ public abstract class BlackboardServletComponent extends ComponentPlugin
    * This method is called when this class is created.
    */
   public void setBindingSite(BindingSite bs) {
+    super.setBindingSite(bs);
     this.serviceBroker = bs.getServiceBroker();
-
-    // kludge until a "AgentIdentificationService" is created
-    if (bs instanceof org.cougaar.core.plugin.PluginBindingSite) {
-      org.cougaar.core.plugin.PluginBindingSite pbs =
-        (org.cougaar.core.plugin.PluginBindingSite) bs;
-      this.agentId = pbs.getAgentIdentifier();
-    } else {
-      throw new RuntimeException(
-          "Unable to get AgentId from bindingSite: " + bs);
-    }
+    this.agentId = getAgentIdentifier();
   }
 
   /**
