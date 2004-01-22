e0aecfc1431b1a150f8aaea496fb7e5d514fddcc
mthome <mthome>
2004-01-22 21:21:25 +0000

    Check for missing BlackboardService in alarms. (cbug 3323)

--- aggagent/src/org/cougaar/lib/aggagent/test/EffortWaster.java
+++ aggagent/src/org/cougaar/lib/aggagent/test/EffortWaster.java
@@ -163,7 +163,10 @@ public class EffortWaster extends ComponentPlugin {
     public synchronized void expire() {
       if (!expired) {
         expired = true;
-        getBlackboardService().signalClientActivity();
+        {
+          org.cougaar.core.service.BlackboardService bbs = getBlackboardService();
+          if (bbs != null) bbs.signalClientActivity();
+        }
       }
     }
     public boolean hasExpired() { return expired; }
